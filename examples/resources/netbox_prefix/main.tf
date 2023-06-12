9aa0b0d8887b850bcd943f69cf25afc324ecd985
Werner Strydom <bloudraak@users.noreply.github.com>
2023-06-12 05:42:58 -0700

    Adds Github Actions, Documentation

---
+++ examples/resources/netbox_prefix/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    netbox = {
+      source = "wernerstrydom/netbox"
+    }
+  }
+}
+
+provider "netbox" {}
+
+resource "netbox_prefix" "this" {
+  prefix = "192.168.3.0/24"
+  description = "This ain't the prefix you're looking for"
+}
+
+output "prefix" {
+  value = netbox_prefix.this
+}
