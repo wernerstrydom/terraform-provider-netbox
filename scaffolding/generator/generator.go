67aadce21c142adf645ba6e2418d11d3e91f3be8
Werner Strydom <bloudraak@users.noreply.github.com>
2023-06-18 09:10:13 -0700

    Supports Config

--- scaffolding/generator/generator.go
+++ scaffolding/generator/generator.go
@@ -6,41 +6,57 @@ import (
     _ "embed"
     "fmt"
     "go/format"
+    "math"
     "os"
     "path/filepath"
     "strings"
     "text/template"
 
     "github.com/iancoleman/strcase"
+    "gopkg.in/yaml.v3"
 )
-
-//go:embed datasource.tmpl
-var datasourceTemplate string
-
-//go:embed datasources.tmpl
-var datasourcesTemplate string
-
-//go:embed resource.tmpl
-var resourceTemplate string
 
 //go:embed *.tmpl
 var templateFS embed.FS
 
 type Configuration struct {
-    Services map[string]Service
+    Services map[string]Service `yaml:"services,omitempty"`
+
+    resourceMap map[string]Resource
 }
 
 type Service struct {
-    Name        string
-    Description string
-    Resources   map[string]Resource
+    Name        string              `yaml:"name,omitempty"`
+    Description string              `yaml:"description,omitempty"`
+    Resources   map[string]Resource `yaml:"resources,omitempty"`
 }
 
 type Resource struct {
-    Name        string
-    Plural      string
-    Description string
-    Attributes  map[string]Attribute
+    Name         string                 `yaml:"name,omitempty"`
+    Plural       string                 `yaml:"plural,omitempty"`
+    Description  string                 `yaml:"description,omitempty"`
+    Attributes   map[string]Attribute   `yaml:"attributes,omitempty"`
+    Associations map[string]Association `yaml:"associations,omitempty"`
+}
+
+type Attribute struct {
+    Name        string `yaml:"name,omitempty"`
+    Description string `yaml:"description,omitempty"`
+    Value       string `yaml:"value,omitempty"`
+    Type        string `yaml:"type,omitempty"`
+    ReadOnly    bool   `yaml:"readonly,omitempty"`
+    MaxLength   int    `yaml:"maxLength,omitempty"`
+    MinLength   int    `yaml:"minLength,omitempty"`
+    Pattern     string `yaml:"pattern,omitempty"`
+    IsKey       bool   `yaml:"key,omitempty"`
+}
+
+type Association struct {
+    Name        string `yaml:"name,omitempty"`
+    Description string `yaml:"description,omitempty"`
+    Type        string `yaml:"type,omitempty"`
+    Min         int    `yaml:"min,omitempty"`
+    Max         int    `yaml:"max,omitempty"`
 }
 
 type TemplateData struct {
@@ -62,17 +78,6 @@ func init() {
     strcase.ConfigureAcronym("API", "api")
     strcase.ConfigureAcronym("SQL", "sql")
     strcase.ConfigureAcronym("ID", "ID")
-}
-
-type Attribute struct {
-    Name        string
-    Description string
-    Type        string
-    ReadOnly    bool
-    MaxLength   int
-    MinLength   int
-    Pattern     string
-    IsKey       bool
 }
 
 func (a Attribute) IsRequired() bool {
@@ -103,6 +108,7 @@ func Generate(outputPath string) error {
                                 Type:        "string",
                                 MaxLength:   100,
                                 MinLength:   1,
+                                Value:       "Test Site",
                             },
                             "slug": {
                                 Name:        "Slug",
@@ -111,6 +117,54 @@ func Generate(outputPath string) error {
                                 MaxLength:   100,
                                 MinLength:   1,
                                 Pattern:     "^[-a-zA-Z0-9_]+$",
+                                Value:       "test-site",
+                            },
+                        },
+                        Associations: map[string]Association{
+                            "prefix": {
+                                Name:        "Prefixes",
+                                Description: "The prefixes assigned to this site.",
+                                Type:        "prefix",
+                                Min:         0,
+                                Max:         math.MaxInt64,
+                            },
+                        },
+                    },
+                },
+            },
+            "ipam": {
+                Name:        "ipam",
+                Description: "IP Address Management",
+                Resources: map[string]Resource{
+                    "prefix": {
+                        Name:        "prefix",
+                        Plural:      "prefixes",
+                        Description: "A prefix represents an assignable range of IP addresses",
+                        Attributes: map[string]Attribute{
+                            "id": {
+                                Name:        "ID",
+                                Description: "The unique numeric ID of the prefix.",
+                                Type:        "int64",
+                                IsKey:       true,
+                                Value:       "123",
+                            },
+                            "prefix": {
+                                Name:        "Prefix",
+                                Description: "The prefix address in CIDR notation.",
+                                Type:        "string",
+                                MaxLength:   43,
+                                MinLength:   1,
+                                Pattern:     "^[0-9a-fA-F.:]+/[0-9]+$",
+                                Value:       "10.0.0.0/24",
+                            },
+                        },
+                        Associations: map[string]Association{
+                            "site": {
+                                Name:        "Site",
+                                Description: "The site to which this prefix is assigned.",
+                                Type:        "site",
+                                Min:         0,
+                                Max:         1,
                             },
                         },
                     },
@@ -118,8 +172,27 @@ func Generate(outputPath string) error {
             },
         },
     }
-    
+
+    c.resourceMap = make(map[string]Resource)
+    for _, service := range c.Services {
+        for _, resource := range service.Resources {
+            c.resourceMap[resource.Name] = resource
+        }
+    }
+
     var err error
+
+    // save the c to a yaml file
+    fout, err := os.Create("config.yaml")
+    if err != nil {
+        return err
+    }
+    defer fout.Close()
+
+    err = yaml.NewEncoder(fout).Encode(c)
+    if err != nil {
+        return err
+    }
 
     dir, err := templateFS.ReadDir(".")
     if err != nil {
@@ -154,6 +227,27 @@ func Generate(outputPath string) error {
                     serviceKey,
                     strcase.ToSnake(resource.Name)+"_resource_gen.go",
                 ),
+                "datasource_example.tmpl": filepath.Join(
+                    outputPath,
+                    "examples",
+                    "data-sources",
+                    "netbox_"+strcase.ToSnake(resource.Name),
+                    "data-source.tf",
+                ),
+                "datasources_example.tmpl": filepath.Join(
+                    outputPath,
+                    "examples",
+                    "data-sources",
+                    "netbox_"+strcase.ToSnake(resource.Plural),
+                    "data-source.tf",
+                ),
+                "resource_example.tmpl": filepath.Join(
+                    outputPath,
+                    "examples",
+                    "resources",
+                    "netbox_"+strcase.ToSnake(resource.Plural),
+                    "main.tf",
+                ),
             }
 
             data := TemplateData{
@@ -162,6 +256,7 @@ func Generate(outputPath string) error {
             }
 
             for templateName, path := range templateMap {
+                fmt.Println("Generating", path)
                 content, err := templateFS.ReadFile(templateName)
                 if err != nil {
                     return err
@@ -178,9 +273,13 @@ func Generate(outputPath string) error {
                     return err
                 }
 
-                formatted, err := format.Source(buf.Bytes())
-                if err != nil {
-                    return err
+                content = buf.Bytes()
+
+                if strings.HasSuffix(path, ".go") {
+                    content, err = format.Source(content)
+                    if err != nil {
+                        return err
+                    }
                 }
 
                 d := filepath.Dir(path)
@@ -195,45 +294,12 @@ func Generate(outputPath string) error {
                     return err
                 }
 
-                _, err = f.Write(formatted)
+                _, err = f.Write(content)
                 if err != nil {
                     return err
                 }
 
             }
-
-            // path = filepath.Join(
-            //     outputPath,
-            //     "internal",
-            //     serviceKey,
-            //     strcase.ToSnake(resource.Name)+"_resource_gen.go",
-            // )
-            // err = writeTemplate(path, resourceTemplate, data)
-            // if err != nil {
-            //     return err
-            // }
-            //
-            // path = filepath.Join(
-            //     outputPath,
-            //     "internal",
-            //     serviceKey,
-            //     strcase.ToSnake(resource.Name)+"_data_source_gen.go",
-            // )
-            // err = writeTemplate(path, datasourceTemplate, data)
-            // if err != nil {
-            //     return err
-            // }
-            //
-            // path = filepath.Join(
-            //     outputPath,
-            //     "internal",
-            //     serviceKey,
-            //     strcase.ToSnake(resource.Plural)+"_data_source_gen.go",
-            // )
-            // err = writeTemplate(path, datasourcesTemplate, data)
-            // if err != nil {
-            //     return err
-            // }
         }
     }
     return nil
