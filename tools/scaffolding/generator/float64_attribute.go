6260e3b4a0aaea144b90daa0991cdf9fc7949d73
Werner Strydom <bloudraak@users.noreply.github.com>
2023-06-24 11:13:21 -0700

    Housekeeping

--- tools/scaffolding/generator/float64_attribute.go
+++ tools/scaffolding/generator/float64_attribute.go
@@ -5,97 +5,97 @@ import "gopkg.in/yaml.v3"
 var _ Attribute = (*Float64Attribute)(nil)
 
 type Float64Attribute struct {
-    name         string
-    description  string
-    value        float64
-    isReadOnly   bool
-    isKey        bool
-    isNullable   bool
-    defaultValue *float64
+	name         string
+	description  string
+	value        float64
+	isReadOnly   bool
+	isKey        bool
+	isNullable   bool
+	defaultValue *float64
 }
 
 func (a *Float64Attribute) IsRequired() bool {
-    return false
+	return false
 }
 
 func (a *Float64Attribute) Name() string {
-    return a.name
+	return a.name
 }
 
 func (a *Float64Attribute) Description() string {
-    return a.description
+	return a.description
 }
 
 func (a *Float64Attribute) Value() float64 {
-    return a.value
+	return a.value
 }
 
 func (a *Float64Attribute) Type() AttributeType {
-    return AttributeTypeFloat64
+	return AttributeTypeFloat64
 }
 
 func (a *Float64Attribute) IsReadOnly() bool {
-    return a.isReadOnly
+	return a.isReadOnly
 }
 
 func (a *Float64Attribute) IsKey() bool {
-    return a.isKey
+	return a.isKey
 }
 
 func (a *Float64Attribute) IsNullable() bool {
-    return a.isNullable
+	return a.isNullable
 }
 
 func (a *Float64Attribute) DefaultValue() any {
-    return a.defaultValue
+	return a.defaultValue
 }
 
 func (a *Float64Attribute) MarshalYAML() (interface{}, error) {
-    var data struct {
-        Name          string        `yaml:"name,omitempty"`
-        Description   string        `yaml:"description,omitempty"`
-        Value         float64       `yaml:"value,omitempty"`
-        AttributeType AttributeType `yaml:"type"`
-        IsReadOnly    bool          `yaml:"readonly,omitempty"`
-        IsKey         bool          `yaml:"key,omitempty"`
-        IsNullable    bool          `yaml:"nullable,omitempty"`
-        DefaultValue  *float64      `yaml:"default,omitempty"`
-    }
+	var data struct {
+		Name          string        `yaml:"name,omitempty"`
+		Description   string        `yaml:"description,omitempty"`
+		Value         float64       `yaml:"value,omitempty"`
+		AttributeType AttributeType `yaml:"type"`
+		IsReadOnly    bool          `yaml:"readonly,omitempty"`
+		IsKey         bool          `yaml:"key,omitempty"`
+		IsNullable    bool          `yaml:"nullable,omitempty"`
+		DefaultValue  *float64      `yaml:"default,omitempty"`
+	}
 
-    data.Name = a.name
-    data.Description = a.description
-    data.Value = a.value
-    data.AttributeType = AttributeTypeFloat64
-    data.IsReadOnly = a.isReadOnly
-    data.IsKey = a.isKey
-    data.IsNullable = a.isNullable
-    data.DefaultValue = a.defaultValue
+	data.Name = a.name
+	data.Description = a.description
+	data.Value = a.value
+	data.AttributeType = AttributeTypeFloat64
+	data.IsReadOnly = a.isReadOnly
+	data.IsKey = a.isKey
+	data.IsNullable = a.isNullable
+	data.DefaultValue = a.defaultValue
 
-    return data, nil
+	return data, nil
 }
 
 func (a *Float64Attribute) UnmarshalYAML(value *yaml.Node) error {
-    var data struct {
-        Name          string        `yaml:"name,omitempty"`
-        Description   string        `yaml:"description,omitempty"`
-        Value         float64       `yaml:"value,omitempty"`
-        AttributeType AttributeType `yaml:"type"`
-        IsReadOnly    bool          `yaml:"readonly,omitempty"`
-        IsKey         bool          `yaml:"key,omitempty"`
-        IsNullable    bool          `yaml:"nullable,omitempty"`
-        DefaultValue  *float64      `yaml:"default,omitempty"`
-    }
+	var data struct {
+		Name          string        `yaml:"name,omitempty"`
+		Description   string        `yaml:"description,omitempty"`
+		Value         float64       `yaml:"value,omitempty"`
+		AttributeType AttributeType `yaml:"type"`
+		IsReadOnly    bool          `yaml:"readonly,omitempty"`
+		IsKey         bool          `yaml:"key,omitempty"`
+		IsNullable    bool          `yaml:"nullable,omitempty"`
+		DefaultValue  *float64      `yaml:"default,omitempty"`
+	}
 
-    if err := value.Decode(&data); err != nil {
-        return err
-    }
+	if err := value.Decode(&data); err != nil {
+		return err
+	}
 
-    a.name = data.Name
-    a.description = data.Description
-    a.value = data.Value
-    a.isReadOnly = data.IsReadOnly
-    a.isKey = data.IsKey
-    a.isNullable = data.IsNullable
-    a.defaultValue = data.DefaultValue
-    return nil
+	a.name = data.Name
+	a.description = data.Description
+	a.value = data.Value
+	a.isReadOnly = data.IsReadOnly
+	a.isKey = data.IsKey
+	a.isNullable = data.IsNullable
+	a.defaultValue = data.DefaultValue
+	return nil
 }
--- tools/scaffolding/generator/string_attribute.go
+++ tools/scaffolding/generator/string_attribute.go
@@ -5,125 +5,125 @@ import "gopkg.in/yaml.v3"
 var _ Attribute = (*StringAttribute)(nil)
 
 type StringAttribute struct {
-    name         string
-    description  string
-    value        string
-    isReadOnly   bool
-    maxLength    int
-    minLength    int
-    pattern      string
-    isKey        bool
-    isNullable   bool
-    defaultValue *string
+	name         string
+	description  string
+	value        string
+	isReadOnly   bool
+	maxLength    int
+	minLength    int
+	pattern      string
+	isKey        bool
+	isNullable   bool
+	defaultValue *string
 }
 
 func (a *StringAttribute) IsRequired() bool {
-    return a.minLength > 0
+	return a.minLength > 0
 }
 
 func (a *StringAttribute) Name() string {
-    return a.name
+	return a.name
 }
 
 func (a *StringAttribute) Description() string {
-    return a.description
+	return a.description
 }
 
 func (a *StringAttribute) Value() string {
-    return a.value
+	return a.value
 }
 
 func (a *StringAttribute) Type() AttributeType {
-    return AttributeTypeString
+	return AttributeTypeString
 }
 
 func (a *StringAttribute) IsReadOnly() bool {
-    return a.isReadOnly
+	return a.isReadOnly
 }
 
 func (a *StringAttribute) MaxLength() int {
-    return a.maxLength
+	return a.maxLength
 }
 
 func (a *StringAttribute) MinLength() int {
-    return a.minLength
+	return a.minLength
 }
 
 func (a *StringAttribute) Pattern() string {
-    return a.pattern
+	return a.pattern
 }
 
 func (a *StringAttribute) IsKey() bool {
-    return a.isKey
+	return a.isKey
 }
 
 func (a *StringAttribute) IsNullable() bool {
-    return a.isNullable
+	return a.isNullable
 }
 
 func (a *StringAttribute) DefaultValue() any {
-    return a.defaultValue
+	return a.defaultValue
 }
 
 func (a *StringAttribute) MarshalYAML() (interface{}, error) {
-    var data struct {
-        Name          string        `yaml:"name,omitempty"`
-        Description   string        `yaml:"description,omitempty"`
-        Value         string        `yaml:"value,omitempty"`
-        AttributeType AttributeType `yaml:"type"`
-        IsReadOnly    bool          `yaml:"readonly,omitempty"`
-        MaxLength     int           `yaml:"maxLength,omitempty"`
-        MinLength     int           `yaml:"minLength,omitempty"`
-        Pattern       string        `yaml:"pattern,omitempty"`
-        IsKey         bool          `yaml:"key,omitempty"`
-        IsNullable    bool          `yaml:"nullable,omitempty"`
-        DefaultValue  *string       `yaml:"default,omitempty"`
-    }
+	var data struct {
+		Name          string        `yaml:"name,omitempty"`
+		Description   string        `yaml:"description,omitempty"`
+		Value         string        `yaml:"value,omitempty"`
+		AttributeType AttributeType `yaml:"type"`
+		IsReadOnly    bool          `yaml:"readonly,omitempty"`
+		MaxLength     int           `yaml:"maxLength,omitempty"`
+		MinLength     int           `yaml:"minLength,omitempty"`
+		Pattern       string        `yaml:"pattern,omitempty"`
+		IsKey         bool          `yaml:"key,omitempty"`
+		IsNullable    bool          `yaml:"nullable,omitempty"`
+		DefaultValue  *string       `yaml:"default,omitempty"`
+	}
 
-    data.Name = a.name
-    data.Description = a.description
-    data.Value = a.value
-    data.AttributeType = AttributeTypeString
-    data.IsReadOnly = a.isReadOnly
-    data.MaxLength = a.maxLength
-    data.MinLength = a.minLength
-    data.Pattern = a.pattern
-    data.IsKey = a.isKey
-    data.IsNullable = a.isNullable
-    data.DefaultValue = a.defaultValue
+	data.Name = a.name
+	data.Description = a.description
+	data.Value = a.value
+	data.AttributeType = AttributeTypeString
+	data.IsReadOnly = a.isReadOnly
+	data.MaxLength = a.maxLength
+	data.MinLength = a.minLength
+	data.Pattern = a.pattern
+	data.IsKey = a.isKey
+	data.IsNullable = a.isNullable
+	data.DefaultValue = a.defaultValue
 
-    return data, nil
+	return data, nil
 }
 
 func (a *StringAttribute) UnmarshalYAML(value *yaml.Node) error {
-    var data struct {
-        Name          string        `yaml:"name,omitempty"`
-        Description   string        `yaml:"description,omitempty"`
-        Value         string        `yaml:"value,omitempty"`
-        AttributeType AttributeType `yaml:"type"`
-        IsReadOnly    bool          `yaml:"readonly,omitempty"`
-        MaxLength     int           `yaml:"maxLength,omitempty"`
-        MinLength     int           `yaml:"minLength,omitempty"`
-        Pattern       string        `yaml:"pattern,omitempty"`
-        IsKey         bool          `yaml:"key,omitempty"`
-        IsNullable    bool          `yaml:"nullable,omitempty"`
-        DefaultValue  *string       `yaml:"default,omitempty"`
-    }
+	var data struct {
+		Name          string        `yaml:"name,omitempty"`
+		Description   string        `yaml:"description,omitempty"`
+		Value         string        `yaml:"value,omitempty"`
+		AttributeType AttributeType `yaml:"type"`
+		IsReadOnly    bool          `yaml:"readonly,omitempty"`
+		MaxLength     int           `yaml:"maxLength,omitempty"`
+		MinLength     int           `yaml:"minLength,omitempty"`
+		Pattern       string        `yaml:"pattern,omitempty"`
+		IsKey         bool          `yaml:"key,omitempty"`
+		IsNullable    bool          `yaml:"nullable,omitempty"`
+		DefaultValue  *string       `yaml:"default,omitempty"`
+	}
 
-    if err := value.Decode(&data); err != nil {
-        return err
-    }
+	if err := value.Decode(&data); err != nil {
+		return err
+	}
 
-    a.name = data.Name
-    a.description = data.Description
-    a.value = data.Value
+	a.name = data.Name
+	a.description = data.Description
+	a.value = data.Value
 
-    a.isReadOnly = data.IsReadOnly
-    a.maxLength = data.MaxLength
-    a.minLength = data.MinLength
-    a.pattern = data.Pattern
-    a.isKey = data.IsKey
-    a.isNullable = data.IsNullable
-    a.defaultValue = data.DefaultValue
-    return nil
+	a.isReadOnly = data.IsReadOnly
+	a.maxLength = data.MaxLength
+	a.minLength = data.MinLength
+	a.pattern = data.Pattern
+	a.isKey = data.IsKey
+	a.isNullable = data.IsNullable
+	a.defaultValue = data.DefaultValue
+	return nil
 }
